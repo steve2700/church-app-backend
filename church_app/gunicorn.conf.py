a0e4c7ea280bf789f4779591d30dd0cca143589e
steve2700 <stevnyaru@github.com>
2024-06-07 21:47:39 +0200

    added the new request oathlib version

--- church_app/gunicorn.conf.py
+++
@@ -1,6 +0,0 @@
-# gunicorn.conf.py
-
-bind = "127.0.0.1:8000"  # IP address and port to bind to
-workers = 3               # Number of worker processes
-timeout = 30              # Timeout for worker processes
-
