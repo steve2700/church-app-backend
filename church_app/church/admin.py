370d7ce5e64474cbc02fe2d5a991d6f21f7309d5
steve2700 <stevnyaru@github.com>
2024-06-13 14:27:26 +0200

    setup up the models for the church

--- church_app/church/admin.py
+++ church_app/church/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Event, Sermon, Member, Series, Scripture, RatingReview, Note
 
-# Register your models here.
+admin.site.register(Event)
+admin.site.register(Sermon)
+admin.site.register(Member)
+admin.site.register(Series)
+admin.site.register(Scripture)
+admin.site.register(RatingReview)
+admin.site.register(Note)
+
